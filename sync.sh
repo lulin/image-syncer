011abfd3281e6eb5b2c9b4e61aeddc65ed6f2d19
lulin <lulin.wang@icloud.com>
2023-05-27 12:34:44 +0800

    debug skopeo sync

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 while read line
 do
   echo ${line}
-  skopeo sync --dest-creds ${USERNAME}:${PASSWORD} --src docker --dest docker registry.k8s.io/pause:3.5.7 registry.aliyuncs.com/wanglulin
+  skopeo sync --dest-creds ${USERNAME}:${PASSWORD} --src docker --dest docker registry.k8s.io/pause:3.5.7 registry.cn-hangzhou.aliyuncs.com/wanglulin
   skopeo sync --dest-creds ${USERNAME}:${PASSWORD} --src docker --dest docker ${line}
 done < sync.list
 
