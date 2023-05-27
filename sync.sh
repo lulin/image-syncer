ab27c0471ad5ae8d5054ee435ae79b918da19357
lulin <lulin.wang@icloud.com>
2023-05-27 12:53:38 +0800

    debug skopeo sync

--- sync.sh
+++ sync.sh
@@ -1,8 +1,9 @@
-for line in $(cat sync.list)
-do
-  echo ${line}
-  echo $line
-  skopeo sync --dest-creds ${USERNAME}:${PASSWORD} --src docker --dest docker $line registry.cn-hangzhou.aliyuncs.com/wanglulin
-done
+IMGS_FILE=$1
+DEST_REGISTRY=$2
+NAMESPACE=$3
 
-exit $?
+for line in $(cat "$IMGS_FILE")
+do
+  echo syncing $line ...
+  skopeo sync --dest-creds ${USERNAME}:${PASSWORD} --src docker --dest docker $line $DEST_REGISTRY/$NAMESPACE
+done
